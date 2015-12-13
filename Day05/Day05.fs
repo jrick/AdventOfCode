589b762e9fece80b36c42b2fc54917be3f75d84f
Josh Rickmar <jrick@devio.us>
2015-12-12 19:02:13 -0500

    Day 5 part 1.

---
+++ Day05/Day05.fs
@@ -0,0 +1,56 @@
+open System.IO
+
+type Requirements = { Vowels : int; Duplicate : bool }
+with
+    static member Initial =
+        {
+            Vowels = 0;
+            Duplicate = false;
+        }
+    member r.Nice =
+        r.Vowels >= 3 && r.Duplicate
+
+let isVowel = function
+    | 'a' | 'e' | 'i' | 'o' | 'u' -> true
+    | _ -> false
+
+let naughtyChars = function
+    | 'a','b' | 'c','d' | 'p','q' | 'x','y' -> true
+    | _ -> false
+
+let rec niceString prevChar (requirements : Requirements) input =
+    match input with
+    | "" -> requirements.Nice
+    | s ->
+        let head = s.[0]
+        let tail = s.Substring(1)
+        let vowelCount =
+            if isVowel head then requirements.Vowels + 1
+            else requirements.Vowels
+        match prevChar with
+        | Some ch when naughtyChars (ch,head) -> false
+        | Some ch ->
+            let newRequirements =
+                { requirements with
+                    Vowels = vowelCount;
+                    Duplicate = requirements.Duplicate || ch = head 
+                }
+            niceString (Some head) newRequirements tail
+        | None ->
+            niceString (Some head) { requirements with Vowels = vowelCount } tail
+
+let challenge1 input =
+    input
+    |> Seq.filter (fun s -> niceString None Requirements.Initial s)
+    |> Seq.length
+    
+module Input =
+    let allStrings =
+        seq { use r = new StreamReader("input.txt")
+            while not r.EndOfStream do yield r.ReadLine() }
+
+[<EntryPoint>]
+let main args =
+    let input = Input.allStrings
+    challenge1 input |> printfn "There are %d nice strings"
+    0
